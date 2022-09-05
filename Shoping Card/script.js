7a3ecb845899e61cfbdd11877c0bb53fab2253f3
Abdessadek El asri <37253593+ElaasriTarik@users.noreply.github.com>
2022-09-05 19:32:11 +0100

    Add files via upload

---
+++ Shoping Card/script.js
@@ -0,0 +1,25 @@
+const t = document.querySelector('h1');
+const btn = document.querySelector('.buy-Btn');
+const price = document.querySelector('.price');
+const list = document.querySelector('ul');
+const img = document.querySelector('img');
+const card = document.querySelector('.card');
+/*
+card.addEventListener('mouseleave', () => {
+  t.classList.add('active-t');
+  img.classList.add('active-img');
+  let b = [btn, price, list].forEach((item) => {
+      item.classList.add('btn-price-ul')
+  });
+})
+
+
+
+card.addEventListener('mouseover', () => {
+     let items = [btn, price, list].forEach((item) => {
+          item.classList.remove('btn-price-ul')
+     });
+     t.classList.remove('active-t');
+     img.classList.remove('active-img');
+})
+*/
